37d57815f09aac47e4ef4c91b82e958c59faff6e
Cesar Murillo <murilloscesar@gmail.com>
2019-05-25 10:24:13 -0500

    Model Product creation

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,30 @@ Route::get('/', [
     'uses' => 'StoreController@getIndex',
     'as' => 'store.index',
 ]);
+
+/*
+Route::get('/', [
+    'uses' => 'ProductController@getIndex',
+    'as' => 'product.index',
+]);
+
+Route::get('/add-to-cart/{id}', [
+    'uses' => 'ProductController@getAddToCart',
+    'as' => 'product.addToCart',
+]);
+
+Route::get('/shopping-cart', [
+    'uses' => 'ProductController@getCart',
+    'as' => 'shop.shopping-cart',
+]);
+
+Route::get('/checkout', [
+    'uses' => 'ProductController@getCheckout',
+    'as' => 'checkout',
+]);
+
+Route::post('/checkout', [
+    'uses' => 'ProductController@postCheckout',
+    'as' => 'checkout',
+]);
+*/
